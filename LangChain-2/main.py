c4f5a409b00c510c847b6a370a187cf524c61a80
Nikhil P Marihal <nikhilpmarihal9@gmail.com>
2026-02-06 08:37:17 +0530

    Add files via upload

---
+++ LangChain-2/main.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_mistralai.chat_models import ChatMistralAI
+
+model = ChatMistralAI(model="mistral-small-latest")
+
+response = model.invoke("Hello")
+print(response)
